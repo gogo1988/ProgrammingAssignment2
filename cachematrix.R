564d6fc4c2483034c10aadd86d6e569cd6796181
gogo1988 <gogo848@sina.com>
2015-10-18 17:12:53 +0200

    assignment 2 is completed

--- cachematrix.R
+++ cachematrix.R
@@ -4,16 +4,16 @@
 ## containinga function to 1) set the value of the Matrix, 2) get the value of the 
 ## matrix, 3)set the inverse of the matrix 4) get the inverse of the matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {  # x is the input matrix that needs to be caluculated for the inverse
         i <- NULL
-        set <- function(y) {
-                x <<- y
+        set <- function(y) {  
+                x <<- y        # set a matrix as the input
                 i <<- NULL
         }
         get <- function() x
-        setInverse <- function(inverse) i <<- inverse
+        setInverse <- function(inverse) i <<- inverse    # arbitrarily set the inverse of a matrix to the variable 'i'
         getInverse <- function() i
-        list(set = set, get = get,
+        list(set = set, get = get,   # construct a list of four functions for the object
              setInverse = setInverse,
              getInverse = getInverse)
 }
@@ -23,15 +23,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ## makeCacheMatrix above. If the inverse has already been calculated (and the matrix 
 ## has not changed), then the cachesolve should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) {    #'x' is the object produced by 'makeCacheMatrix' function
         ## Return a matrix that is the inverse of 'x'
         i <- x$getInverse()
         if(!is.null(i)) {
                 message("getting cached the calculated inverse")
-                return(i)
+                return(i)  # 'i' already contains a inversed matrix set by 'setInverse' function
         }
         data <- x$get()
-        i <- solve(data, ...)
+        i <- solve(data, ...)  # calculate the inverse for the matrix contained in the object produced by 'makeCacheMatrix'
         x$setInverse(i)
-        i
+        i     # 'i' stores the inverse of the matrix
 }
